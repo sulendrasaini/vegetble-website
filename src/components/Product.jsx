854ad0e81a8f1a7bb5152aff76e100f089c1bea4
rinki pc <sulendrasrj@gmail.com>
2026-04-24 13:47:29 +0530

    new File

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,83 @@
 import React, { useState } from 'react'
 import Headings from './heading/Headings'
+import ProductsList from '../components/productList/ProductList.js'
+import Button from "../components/button/Button.jsx"
+import { BsHeartFill } from 'react-icons/bs'
+import { PiPlus } from 'react-icons/pi'
+
+import { FaPlus } from "react-icons/fa";
 
 const Product = () => {
 
     const [isActive, setIsActive] = useState("All")
 
+   const [likedItems, setLikedItems] = useState([])
 
-  return (
-    <section>
-        <Headings highLight="Our" normalWord="Products"/>
-        <div className='max-w-[1400px] py-20 px-10 mx-auto'>
+   
+   
+   const renderLists = ProductsList.map((product) => {
 
-            {/* Products Tabs */}
-            <div className='flex justify-center gap-2 '>{tabs.map((tab)=>(
-                 <button key={tab}className={`text-lg font-medium  px-6 py-2 rounded-lg  ${isActive === tab ? "text-white bg-gradient-to-b from-green-600 to-green-900" : "bg-zinc-200" } transition-all duration-300`}  onClick={()=>setIsActive(tab)} >
-                    {tab}
-                 </button>
+        const toggleLike = (id) =>{
 
-            ))}</div>
-        </div>
-    </section>
-  )
+            if(likedItems.includes(id)) {
+               setLikedItems( likedItems.filter((item) => item !==id))
+            } else {
+                setLikedItems([...likedItems, id])
+            }
+        }
+               
+     
+        
+    //    const color = ()=>{
+    //      return setColorChanged(!colorChanged)
+    //    };
+     
+        return (
+            <div className='w-[30vh] bg-zinc-200 rounded-lg ' key={product.id}>
+
+                {/* Heart & Plus Icon  */}
+                <div className='flex justify-between px-4 py-3'>
+                    <button className={`text-2xl cursor-pointer ${likedItems.includes(product.id) ? "text-red-600": "bg-zinc-200"}`} onClick={()=>toggleLike(product.id)} ><BsHeartFill /></button>
+                    <button className='text-white text-lg p-2 py-1 rounded cursor-pointer bg-green-600'><FaPlus /></button>
+                </div>
+ 
+                  {/* Card Content  */}
+                <div className='flex flex-col gap-2 items-center'>
+
+                    {/* Image  */}
+                    <div className='px-3'>
+                        <img src={product.image} alt="image" className='w-[40vh] h-[20vh] ' />
+                    </div>
+                    {/* Content  */}
+                    <h3 className='text-lg font-bold text-zinc-700 cursor-default'>{product.title}</h3>
+                    <span className='font-bold cursor-default'>{product.price}</span>
+                    <div className='p-5 '> <Button content="Shop Now" /></div>
+                   
+                </div>
+            </div>
+        )
+    });
+
+
+    return (
+        <section>
+            <Headings highLight="Our" normalWord="Products" />
+            <div className='max-w-[1400px] py-20 px-10 mx-auto'>
+
+                {/* Products Tabs */}
+                <div className='flex justify-center gap-2 py-20'>{tabs.map((tab) => (
+                    <button key={tab} className={`text-lg font-medium  px-6 py-2 rounded-lg cursor-pointer ${isActive === tab ? "text-white bg-gradient-to-b from-green-600 to-green-900" : "bg-zinc-200"} transition-all duration-300`} onClick={() => setIsActive(tab)} >
+                        {tab}
+                    </button>
+
+                ))}</div>
+
+                {/* Product List  */}
+
+                <div className='flex gap-5 flex-wrap justify-around'>{renderLists}</div>
+            </div>
+        </section>
+    )
 }
 
 export default Product;
---
+++ src/components/productList/ProductList.js
@@ -0,0 +1,213 @@
+import strawberry from "../../assets/image/strawberry.png"
+import butter from "../../assets/image/butter.png"
+import eggs from "../../assets/image/eggs.png"
+import cabbage from "../../assets/image/cabbage.png"
+import Eggplant from "../../assets/image/eggplant.png"
+import Shrimp from "../../assets/image/shrimp.png"
+import Kiwi from "../../assets/image/kiwi.png"
+import Capsicum from "../../assets/image/capsicum.png"
+import Broccoli from "../../assets/image/broccoli.png"
+import Yogurt from "../../assets/image/yogurt.png"
+import Beef from "../../assets/image/beef.png"
+import ChickenBreast from "../../assets/image/chicken-breast.png"
+import Kale from "../../assets/image/kale.png"
+import Cheese from "../../assets/image/cheese.png"
+import CondensedMilk from "../../assets/image/condensed-milk.png"
+import Salmon from "../../assets/image/salmon.png"
+import Banana from "../../assets/image/banana.png"
+import Milk from "../../assets/image/milk.png"
+import Pineapple from "../../assets/image/pineapple.png"
+import Tofu from "../../assets/image/Tofu.png"
+import Tilapia from "../../assets/image/tilapia.png"
+import SliceCheese from "../../assets/image/slice-cheese.png"
+import RicottaCheese from "../../assets/image/ricotta-cheese.png"
+import Grapes from "../../assets/image/Grapes.png"
+import Lettuce from "../../assets/image/lettuce.png"
+
+
+
+const products = [
+   {
+        id: 1,
+        image: strawberry,
+        title: "Strawberry",
+        price: "$3.00",
+        category: "Fruits"
+    },
+    {
+        id:2,
+        image: butter,
+        title: "Butter",
+        price: "$3.50",
+        category: "Dairy"
+    },
+    {
+        id:3,
+        image: eggs,
+        title: "Eggs",
+        price: "$2.05",
+        category : "Dairy"
+    },
+    {
+        id:4,
+        image: cabbage,
+        title: "Cabbage",
+        price: "$3.00",
+        category: "vagetable"
+    },
+    {
+         id:5,
+        image: Eggplant,
+        title: "Eggplant",
+        price: "$3.00",
+        category: "vagetable"
+    },
+    {
+         id:6,
+        image: Shrimp,
+        title: "Shrimp",
+        price: "$3.00",
+        category: "Sead Food"
+    },
+    {
+         id:7,
+        image: Kiwi,
+        title: "Kiwi",
+        price: "$3.00",
+        category: "Fruit"
+
+    },
+
+     {
+         id:8,
+        image: Capsicum,
+        title: "Capsicum",
+        price: "$3.00",
+        category: "vagetable"
+    },
+    {
+         id:9,
+        image: Broccoli,
+        title: "Broccoli",
+        price: "$3.00",
+        category: "Vagetable"
+
+    },
+    {
+         id:10,
+        image: Yogurt,
+        title: "Yogurt",
+        price: "$3.00",
+        category: "Dairy"
+    },
+    {
+         id:11,
+        image: Beef,
+        title: "Beef",
+        price: "$3.00",
+        category: "Sea Food"
+    },
+     {
+         id:12,
+        image: ChickenBreast,
+        title: "CkickenBreast",
+        price: "$3.00",
+        category: "Sea Food"
+    },
+     {
+         id:13,
+        image: Kale,
+        title: "Kale",
+        price: "$3.00",
+        category: "Sea Food"
+    },
+     {
+         id:14,
+        image: Cheese,
+        title: "Cheese",
+        price: "$3.00",
+        category: "Dairy"
+
+    },
+     {
+         id:15,
+        image: CondensedMilk,
+        title: "CondensedMik",
+        price: "$3.00",
+        category: "vagetable"
+
+    },
+     {
+         id:16,
+        image: Salmon,
+        title: "Salmon",
+        price: "$3.00",
+        category: "Sea Food"
+    },
+    {
+         id:17,
+        image: Banana,
+        title: "Banana",
+        price: "$3.00",
+        category: "Fruits"
+    },
+    {
+         id:18,
+        image: Milk,
+        title: "Milk",
+        price: "$3.00",
+        category: "Dairy"
+    },
+    {
+         id:19,
+        image: Pineapple,
+        title: "pineapple",
+        price: "$3.00",
+        category: "Fruit"
+    },
+    {
+         id:20,
+        image: Tofu,
+        title: "Tofu",
+        price: "$3.00",
+        category: "Dairy"
+    },
+    {
+         id:21,
+        image: Tilapia,
+        title: "Tilapia",
+        price: "$3.00",
+        category:"Sea Food"
+    },
+     {
+         id:22,
+        image: SliceCheese,
+        title: "SliceCheese",
+        price: "$3.00",
+        category : "Dairy"
+    },
+     {
+         id:23,
+        image: RicottaCheese,
+        title: "RicottaCheese",
+        price: "$3.00",
+        category: "Dairy"
+    },
+     {
+         id:24,
+        image: Grapes,
+        title: "Grapes",
+        price: "$3.00",
+        category: "Fruits"
+
+    },
+     {
+         id:25,
+        image: Lettuce,
+        title: "Lettuce",
+        price: "$3.00",
+        category: "vagetable"
+    },
+]
+
+export default products;
